1c780e295e2341f392891253cd10e932e7ff2b76
Ryan <r@goodestlab.com>
2020-12-15 05:04:31 +0800

    Update resolvers.js

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,35 +1,34 @@
 const DB = process.env.DB_DATABASE;
 const { MongoClient, Logger, ObjectId } = require('mongodb');
-// const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}`;
-// const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
-// const connection = client.connect();
+const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}`;
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = client.connect();
 
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-var collections = ['users']
-// connection.then(async (db) => {
-// 	const exist = await db.db(DB).listCollections().toArray();
-// 	var array = [];
-// 	exist.forEach(e=>array.push(e['name']))
-// 	collections.forEach(c=>!array.includes(c) && db.db(DB).createCollection(c)) 
-// });
+var collections = ['test-users']
+connection.then(async (db) => {
+	const exist = await db.db(DB).listCollections().toArray();
+	var array = [];
+	exist.forEach(e=>array.push(e['name']))
+	collections.forEach(c=>!array.includes(c) && db.db(DB).createCollection(c)) 
+});
 
 module.exports = {
 	Query: {
 		user: (root, input) => {
-			return {name:"hello"}
-		// 	if (input._id) {input._id = new ObjectId(input._id)};
-		// 	return new Promise((resolve) => {
-		// 		connection.then((db) => {
-		// 			db.db(DB).collection('users').findOne(
-		// 				Object.assign(input, {
-		// 					is_deleted:false
-		// 				}), (err, res) => {
-		// 				resolve(res);
-		// 			});
-		// 		});
-		// 	});
+			if (input._id) {input._id = new ObjectId(input._id)};
+			return new Promise((resolve) => {
+				connection.then((db) => {
+					db.db(DB).collection('users').findOne(
+						Object.assign(input, {
+							is_deleted:false
+						}), (err, res) => {
+						resolve(res);
+					});
+				});
+			});
 		},
 	},
 	Mutation: {
@@ -69,34 +68,34 @@ module.exports = {
 		// 		}
 
 		// },
-		// register: async (root, input) => {
-		// 	var command = [];
-		// 	Object.keys(input).forEach(function (key) {
-		// 		obj = {}; obj[key] = input[key]; command.push(obj)
-		// 	});
-		// 	var checkExist = await new Promise((resolve) => {
-		// 		connection.then((db) => {
-		// 			db.db(DB).collection('users').find({ $or: command }).toArray((err, res) => {
-		// 				resolve(res);
-		// 			});
-		// 		});
-		// 	});
-		// 	if (checkExist.length) {throw new Error('Number been used')}
-		// 	if(input.password){input.password = await bcrypt.hash(input.password, 10)};
-		// 	return new Promise((resolve) => {
-		// 		connection.then((db) => {
-		// 			db.db(DB).collection('users').insertOne(
-		// 				Object.assign(input, {
-		// 					created_at: new Date(),
-		// 					updated_at: new Date(),
-		// 					last_login: null,
-		// 					is_deleted: false
-		// 				}), (err, res) => {
-		// 					resolve(res.ops[0]);
-		// 				});
-		// 		});
-		// 	});
-		// },
+		register: async (root, input) => {
+			var command = [];
+			Object.keys(input).forEach(function (key) {
+				obj = {}; obj[key] = input[key]; command.push(obj)
+			});
+			var checkExist = await new Promise((resolve) => {
+				connection.then((db) => {
+					db.db(DB).collection('users').find({ $or: command }).toArray((err, res) => {
+						resolve(res);
+					});
+				});
+			});
+			if (checkExist.length) {throw new Error('Number been used')}
+			if(input.password){input.password = await bcrypt.hash(input.password, 10)};
+			return new Promise((resolve) => {
+				connection.then((db) => {
+					db.db(DB).collection('users').insertOne(
+						Object.assign(input, {
+							created_at: new Date(),
+							updated_at: new Date(),
+							last_login: null,
+							is_deleted: false
+						}), (err, res) => {
+							resolve(res.ops[0]);
+						});
+				});
+			});
+		},
 		// deleteUser: async (root, input) => {
 		// 	if (input._id) {input._id = new ObjectId(input._id)};
 		// 	var user =  await new Promise((resolve) => {
